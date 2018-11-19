eca5a108dffe7aae877ff4b466f5095c0446a32c
dede <winsandymyint@gmail.com>
2018-11-19 11:50:38 +0800

    Add register link

--- src/components/login.js
+++ src/components/login.js
@@ -1,80 +1,98 @@
 import React, { Component } from 'react'
-import {Row, Col, Form, Icon, Input, Button } from 'antd';
+import { NavLink } from 'react-router-dom'
+import { Row, Col, Form, Icon, Input, Button } from 'antd'
 import PropTypes from 'prop-types'
 
-const FormItem = Form.Item;
+const FormItem = Form.Item
 
 class Login extends Component {
-
-    componentWillReceiveProps = (nextProps) => {
-      const { user } = nextProps;
-      if (user && user.id) {
-        this.props.history.push('/')
-      }
+  componentWillReceiveProps = nextProps => {
+    const { user } = nextProps
+    if (user && user.id) {
+      this.props.history.push('/')
     }
+  }
 
-    handleFormSubmit = (email, password) => {
-        this.props.login(email, password)
-    }
+  handleFormSubmit = (email, password) => {
+    this.props.login(email, password)
+  }
 
-    render() {
-        return (
-            <Row align='middle' className='login' justify='center' type='flex'>
-              <Col className='login__form' span={6}>
-                <WrappedNormalLoginForm onSubmit={this.handleFormSubmit} />
-              </Col>
-            </Row>
-        )
-    }
+  render() {
+    return (
+      <Row align='middle' className='login' justify='center' type='flex'>
+        <Col className='login__form' span={6}>
+          <WrappedNormalLoginForm onSubmit={this.handleFormSubmit} />
+        </Col>
+      </Row>
+    )
+  }
 }
 
 class LoginForm extends React.Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
+  handleSubmit = e => {
+    e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
         const { email, password } = values
         this.props.onSubmit(email, password)
       }
-    });
+    })
   }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { getFieldDecorator } = this.props.form
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <h2 className="form-header">Login</h2>
+      <Form onSubmit={this.handleSubmit} className='login-form'>
+        <h2 className='form-header'>Login</h2>
         <FormItem>
           {getFieldDecorator('email', {
-            rules: [{ required: true, message: 'Please input your email!' }],
+            rules: [{ required: true, message: 'Please input your email!' }]
           })(
-            <Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" />
+            <Input
+              prefix={<Icon type='mail' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder='Email'
+            />
           )}
         </FormItem>
         <FormItem>
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+            rules: [{ required: true, message: 'Please input your Password!' }]
           })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            <Input
+              prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type='password'
+              placeholder='Password'
+            />
           )}
         </FormItem>
         <FormItem>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
+          <Row>
+            <Col span={12}>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='login-form-button'
+              >
+                Log in
+              </Button>
+            </Col>
+            <Col span={12} align='right'>
+              <NavLink to='/register'>Register for loan</NavLink>
+            </Col>
+          </Row>
         </FormItem>
       </Form>
-    );
+    )
   }
 }
 
-const WrappedNormalLoginForm = Form.create()(LoginForm);
+const WrappedNormalLoginForm = Form.create()(LoginForm)
 
 Login.propTypes = {
-    handleSubmit: PropTypes.func,
-    history: PropTypes.object,
-    isLoadingLogin: PropTypes.bool,
-    login: PropTypes.func,
-    submitting: PropTypes.bool
+  handleSubmit: PropTypes.func,
+  history: PropTypes.object,
+  isLoadingLogin: PropTypes.bool,
+  login: PropTypes.func,
+  submitting: PropTypes.bool
 }
 export default Login
--- src/components/register.js
+++ src/components/register.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import {Row, Col, Form, Input, Button, Select, InputNumber } from 'antd';
+import {Row, Col, Form, Input, Button, Select, InputNumber } from 'antd'
 import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
 import { message as AntdMessage } from 'antd'
 import noop from 'lodash/noop'
 
-const FormItem = Form.Item;
-const Option = Select.Option;
+const FormItem = Form.Item
+const Option = Select.Option
 
 const triggerMessage = (type, content = '[ no content ]', duration = 3, onClose = noop) =>
     AntdMessage[type](content, duration, onClose)
@@ -37,56 +38,56 @@ class Register extends Component {
 
 class RegisterForm extends React.Component {
   handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
         this.props.onSubmit(values)
       }
-    });
+    })
   }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const { getFieldDecorator } = this.props.form
     const formItemLayout = {
       labelCol: { span: 6 },
       wrapperCol: { span: 14 },
-    };
+    }
     return (
-      <Form onSubmit={this.handleSubmit} className="register-form">
-        <h2 className="form-header">Get a business loan in 24 hours</h2>
+      <Form onSubmit={this.handleSubmit} className='register-form'>
+        <h2 className='form-header'>Get a business loan in 24 hours</h2>
         <FormItem
           {...formItemLayout}
-          label="Name"
+          label='Name'
         >
           {getFieldDecorator('name', {
             rules: [{ required: true, message: 'Please input your name!' }],
           })(
-            <Input placeholder="Name" />
+            <Input placeholder='Name' />
           )}
         </FormItem>
         <FormItem
           {...formItemLayout}
-          label="Email"
+          label='Email'
         >
           {getFieldDecorator('email', {
             rules: [{ required: true, message: 'Please input your email!' }],
           })(
-            <Input placeholder="Email" />
+            <Input placeholder='Email' />
           )}
         </FormItem>
         <FormItem
           {...formItemLayout}
-          label="Password"
+          label='Password'
           >
           {getFieldDecorator('password', {
             rules: [{ required: true, message: 'Please input your Password!' }],
           })(
-            <Input type="password" placeholder="Password" />
+            <Input type='password' placeholder='Password' />
           )}
         </FormItem>
         <FormItem
           {...formItemLayout}
-          label="Loan Type"
+          label='Loan Type'
           hasFeedback
         >
           {getFieldDecorator('loanType', {
@@ -94,15 +95,15 @@ class RegisterForm extends React.Component {
               { required: true, message: 'Please select your loan type!' },
             ],
           })(
-            <Select placeholder="Please select a loan type">
-              <Option value="personal">Personal Loan</Option>
-              <Option value="business">Business</Option>
+            <Select placeholder='Please select a loan type'>
+              <Option value='personal'>Personal Loan</Option>
+              <Option value='business'>Business</Option>
             </Select>
           )}
         </FormItem>
         <FormItem
           {...formItemLayout}
-          label="Amount"
+          label='Amount'
         >
           {getFieldDecorator('amount', {
             rules: [{ required: true, message: 'Please input your required loan amount!' }],
@@ -117,21 +118,21 @@ class RegisterForm extends React.Component {
         <FormItem
           wrapperCol={{ span: 12, offset: 6 }}
         >
-          <Button type="primary" htmlType="submit" className="register-form-button">Register</Button>
-          <div style={{ width: '100%' }}>Or <a href="/login">Login now!</a></div>
+          <Button type='primary' htmlType='submit' className='register-form-button'>Register</Button>
+          <div style={{ width: '100%' }}>Or <NavLink to='/login'>
+              Login now!
+            </NavLink>
+          </div>
         </FormItem>
       </Form>
-    );
+    )
   }
 }
 
-const WrappedNormalRegisterForm = Form.create()(RegisterForm);
+const WrappedNormalRegisterForm = Form.create()(RegisterForm)
 
 Register.propTypes = {
     handleSubmit: PropTypes.func,
     history: PropTypes.object,
-    isLoadingLogin: PropTypes.bool,
-    login: PropTypes.func,
-    submitting: PropTypes.bool
 }
 export default Register
